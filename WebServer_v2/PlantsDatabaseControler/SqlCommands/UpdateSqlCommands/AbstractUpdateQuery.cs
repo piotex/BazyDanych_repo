daad6bf834376973f7537216a40da00cb2915eba
piotex <pkubon2@gmail.com>
2022-05-27 17:28:01 +0200

    Added optipon to run AddActualTasks Procedure

---
+++ WebServer_v2/PlantsDatabaseControler/SqlCommands/UpdateSqlCommands/AbstractUpdateQuery.cs
@@ -0,0 +1,14 @@
+﻿using DatabaseConneciton;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlantsDatabaseControler.SqlCommands.InsertSqlCommands
+{
+    public abstract class AbstractUpdateQuery<T> : SqlCommand
+    {
+        public abstract string GetUpdateQuery(T t);
+    }
+}
---
+++ WebServer_v2/PlantsDatabaseControler/SqlCommands/UpdateSqlCommands/UpdateUserIdActualTaskQuery.cs
@@ -0,0 +1,16 @@
+﻿using DatabaseConneciton;
+using System;
+
+namespace PlantsDatabaseControler.SqlCommands.InsertSqlCommands
+{
+    public class UpdateUserIdActualTaskQuery : AbstractUpdateQuery<ActualTask>
+    {
+        public override string GetUpdateQuery(ActualTask user)
+        {
+            return string.Format(
+                            "UPDATE ActualTask SET UserID = {0} WHERE ActualTaskID = {1} ",
+                            user.USERID, user.ACTUALTASKID
+                            );
+        }
+    }
+}
